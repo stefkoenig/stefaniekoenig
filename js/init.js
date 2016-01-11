2c92f1e6d4711a17db403e93c75e3f94cb7aab59
StefKoenig <stefanie.l.koenig@gmail.com>
2016-01-10 17:53:10 -0800

    added descriptions and links and images for all 4 projects. Added about me footer and chevron down arrow in jumbotron

--- js/init.js
+++ js/init.js
@@ -3,9 +3,5 @@
 
     $('.button-collapse').sideNav();
 
-    $(document).ready(function(){
-         $('.slider').slider({full_width: true});
-       });
-       
   }); // end of document ready
 })(jQuery); // end of jQuery name space
