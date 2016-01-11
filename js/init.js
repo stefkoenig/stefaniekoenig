8d03a422cab28f85b66017b9162883c619c36f24
StefKoenig <stefanie.l.koenig@gmail.com>
2016-01-10 16:39:19 -0800

    styled top of page and added a slider which i may remove later

--- js/init.js
+++ js/init.js
@@ -3,5 +3,9 @@
 
     $('.button-collapse').sideNav();
 
+    $(document).ready(function(){
+         $('.slider').slider({full_width: true});
+       });
+       
   }); // end of document ready
 })(jQuery); // end of jQuery name space
